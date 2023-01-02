d2428921b81d0dcf7fa62fee8217b8add82cca7a
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-02 21:34:11 +0000

    quiz app project html done

---
+++ quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: 'How old Rodrigo is?',
+    a: '25',
+    b: '27',
+    c: '29',
+    d: '31',
+    correct: 'c'
+  },
+  {
+    question: 'Where does Rodrigo Live?',
+    a: 'Ireland',
+    b: 'Portugal',
+    c: 'Brazil',
+    d: 'USA',
+    correct: 'b'
+  }, 
+  {
+    question: 'Where Rodrigo was born?',
+    a: 'Brazil',
+    b: 'Australia',
+    c: 'Netherlands',
+    d: 'Portugal',
+    correct: 'a'
+  },
+  {
+    question: 'Witch country Rodrigo likes more?',
+    a: 'Brazil',
+    b: 'Ireland',
+    c: 'New Zealand',
+    d: 'Spain',
+    correct: 'c'
+  },
+  {
+    question: 'Where Rodrigo did his English exchange?',
+    a: 'USA',
+    b: 'Australia',
+    c: 'Ireland',
+    d: 'Malta',
+    correct: 'c'
+  }
+]
